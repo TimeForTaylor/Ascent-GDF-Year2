cfbe6913ed86da910d0fd78edf17b9d8d74397ca
MUA756 <t.lambert1@edu.salford.ac.uk>
2021-12-08 10:26:50 +0000

    second commit

---
+++ Assets/script/GameControl Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider pickup)
+    {
+        if (pickup.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
